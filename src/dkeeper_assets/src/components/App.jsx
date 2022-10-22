f22660c7e62a8fc0e5bf3c4d4ea8f6d7bdab79bd
anderson <anderson.mends@gmail.com>
2022-10-22 18:35:04 -0300

    final project

--- src/dkeeper_assets/src/components/App.jsx
+++ src/dkeeper_assets/src/components/App.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { dkeeper } from "../../../declarations/dkeeper";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+      dkeeper.createNote(newNote.title, newNote.content)
+      return [newNote, ...prevNotes];
     });
+  };
+
+  useEffect(() => {
+    fetchData();
+
+  }, []);
+
+  async function fetchData(params) {
+    const notesArray = await dkeeper.readNotes();
+    setNotes(notesArray);
   }
 
   function deleteNote(id) {
+    dkeeper.removeNote(id);
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
